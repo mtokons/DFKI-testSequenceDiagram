6c85b369ba3fdd768fb9394c8c6088f975eaca5c
mhasnain <md.hasnain@dfki.de>
2020-02-19 14:04:48 +0100

    try different source

--- mermaid/src/App.js
+++ mermaid/src/App.js
@@ -9,13 +9,21 @@ sequenceDiagram
   John-->>-Alice: Hi Alice, I can hear you!
   John-->>-Alice: I feel great!
 `
+const graph2 = `
+pie title Pets adopted by volunteers
+	"Dogs" : 386
+	"Cats" : 85
+	"Rats" : 15
+`
+// const graph3 =fetch('/test.txt')
 
 
 
 function App() {
   return (
     <div className="App">
-      <Mermaid id="graph1" content={graph} />
+      <Mermaid id="graph" content={graph} />
+      <Mermaid id="graph2" content={graph2} />
     </div>
   );
 }
